bc0dd427fbb1d6eebd401eebcfc17fefb6d1e5a7
yuki-0202 <stoicbazooka@gmail.com>
2022-01-27 20:25:27 +0900

    バリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presence: true do
-    validates :nickname
-    validates :last_name
-    validates :first_name
-    validates :last_name_kana
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
+
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
+    validates :last_name 
+    validates :first_name 
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' } do
+    validates :last_name_kana 
     validates :first_name_kana
-    validates :birthday
   end
 end
