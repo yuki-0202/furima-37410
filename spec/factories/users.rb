6e39ebfe35ab650f9aaa51e03c69b5137da76929
yuki-0202 <stoicbazooka@gmail.com>
2022-01-28 18:15:03 +0900

    userモデルのテスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :user do
+    transient do
+      person { Gimei.name }
+    end
+    nickname {Faker::Name.name}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    last_name { person.last.kanji }
+    first_name { person.first.kanji }
+    last_name_kana { person.last.katakana }
+    first_name_kana { person.first.katakana }
+    birthday { Faker::Date.birthday(min_age:18, max_age:65)}
+  end
+end
