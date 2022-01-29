92877873bff99aab9d19db45c39a8c64e44a13de
yuki-0202 <stoicbazooka@gmail.com>
2022-01-29 16:40:07 +0900

    Fakerのpassword生成の際、必ず英数字を含むように修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     end
     nickname { Faker::Name.name }
     email { Faker::Internet.free_email }
-    password = Faker::Internet.password(min_length: 6)
+    password = '1a' + Faker::Internet.password(min_length: 4)
     password { password }
     password_confirmation { password }
     last_name { person.last.kanji }
