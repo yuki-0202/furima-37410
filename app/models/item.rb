eda044189c15add7e1d6b5e70d754898273eba5b
yuki-0202 <stoicbazooka@gmail.com>
2022-01-30 20:02:29 +0900

    ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
   belongs_to :user
 end
