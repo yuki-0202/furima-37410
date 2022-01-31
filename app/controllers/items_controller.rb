7dbe68988691c1ee70b623a4f9b386b359f49295
yuki-0202 <stoicbazooka@gmail.com>
2022-01-31 11:19:50 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,26 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(
+      :name,
+      :info,
+      :category_id,
+      :status_id,
+      :shipping_fee_status_id,
+      :prefecture_id,
+      :scheduled_delivery_id,
+      :price
+      ).merge(user_id: current_user.id)
+  end
 end
