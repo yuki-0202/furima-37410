cd49739158e42abcbf933c9b72e2847baa6a93ff
yuki-0202 <stoicbazooka@gmail.com>
2022-02-01 16:04:34 +0900

    「authenticate_user!」を「before_action」を用いて実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,10 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
   end
 
   def new
-    authenticate_user!
     @item = Item.new
   end
 
