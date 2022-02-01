279b97af04cbb3db143372f54ba793f31141fa11
yuki-0202 <stoicbazooka@gmail.com>
2022-02-01 16:07:06 +0900

    テスト項目を追加（ユーザーが紐付いていなければ出品できない）

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -80,6 +80,11 @@ RSpec.describe Item, type: :model do
         @item.price = '１２３'
         valid(@item)
       end
+      it 'ユーザーが紐付いていなければ出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
     end
   end
 end
