294b9b018f5c8461ce211805b36208fdfe1688d5
yuki-0202 <stoicbazooka@gmail.com>
2022-02-05 23:59:17 +0900

    バリデーションを修正

--- app/models/sale_record_destination.rb
+++ app/models/sale_record_destination.rb
@@ -14,8 +14,9 @@ class SaleRecordDestination
   end
 
   validates :phone_number, presence: true
-  validates :phone_number, format: { with: /\A.{10,11}\z/, message: 'is too short' }
-  validates :phone_number, format: { with: /\A[0-9]+\z/, message: ' is invalid. Input only number' }
+  validates :phone_number, length: { minimum: 10, message: 'is too short' }
+  validates :phone_number, length: { maximum: 11, message: 'is too long' }
+  validates :phone_number, numericality: { with: /\A[0-9]+\z/, message: ' is invalid. Input only number' }
 
   def save
     sale_record = SaleRecord.create(user_id: user_id, item_id: item_id)
