e32092a608c53eb376f2d98fae8199ec2667804f
yuki-0202 <stoicbazooka@gmail.com>
2022-02-05 16:03:48 +0900

    バリデーションの修正

--- app/models/sale_record_destination.rb
+++ app/models/sale_record_destination.rb
@@ -4,13 +4,16 @@ class SaleRecordDestination
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :address
-    validates :phone_number, numericality: { with: /\A[0-9]{10,11}\z/, message: "is too short" }
     validates :user_id
     validates :item_id
   end
+
+  validates :phone_number, presence: true
+  validates :phone_number, format: { with: /\A.{10,11}\z/, message: "is too short" }
+  validates :phone_number, format: { with: /\A[0-9]+\z/, message: " is invalid. Input only number" }
 
   def save
     sale_record = SaleRecord.create(user_id: user_id, item_id: item_id)
